74fd6ebee6dcb5ef598e1875fa49bbabe258f088
A01711723 <a01711723@tec.mx>
2024-10-14 22:16:14 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,54 +1,54 @@
-/*** Angelo segura Ibarra
-    A01711723
-    Fecha:18/09/2024
-***/
+/*
+Angelo Segura Ibarra
+14/10/2024
+*/
+
+
 #include <iostream>  
 #include <string> 
 #include "cancion.h"
-#include "sorts.h" 
-#include <vector>
+#include "AVL.h" 
 #include <sstream>
 #include <limits>
 #include <fstream>
 
-int main() {
+int main() { 
 
-    Sorts sorter;
+    AVL<Cancion> arbol;
 
     std::ifstream archivo("Lista_canciones.txt");  // Abrimos el archivo a leer   
     if(archivo.is_open()){
-    std::string linea;
-    // Saltar la primera línea de encabezado
-    std::getline(archivo, linea);
+        std::string linea;
+        // Saltar la primera línea de encabezado
+        std::getline(archivo, linea);
 
-    // Leer cada línea del archivo
-    while (std::getline(archivo, linea)) {
-        std::stringstream ss(linea);
-        std::string nombre, artista, genero;
-        float vistas, duracion;
-        int lanzamiento;
+        // Leer cada línea del archivo
+        while (std::getline(archivo, linea)) {
+            std::stringstream ss(linea);
+            std::string nombre, artista, genero;
+            float vistas, duracion;
+            int lanzamiento;
 
-        // Leer y separar los valores para seleccionar nuestrosa atributos 
-        std::getline(ss, nombre, ',');
-        std::getline(ss, artista, ',');
-        std::getline(ss, genero, ',');
-        ss >> vistas; ss.ignore(1);  // Leer vistas y saltar la coma
-        ss >> lanzamiento; ss.ignore(1);  
-        ss >> duracion;  
+            // Leer y separar los valores para seleccionar nuestros atributos 
+            std::getline(ss, nombre, ',');
+            std::getline(ss, artista, ',');
+            std::getline(ss, genero, ',');
+            ss >> vistas; ss.ignore(1);  // Leer vistas y saltar la coma
+            ss >> lanzamiento; ss.ignore(1);  
+            ss >> duracion;  
 
-        // Crear un objeto Cancion usando el constructor 
-        Cancion cancion(nombre, artista, genero, vistas, lanzamiento, duracion);
-        sorter.agregar_cancion(cancion);  // Agregamos todo al vevtor
+            // Crear un objeto Cancion usando el constructor 
+            Cancion cancion(nombre, artista, genero, vistas, lanzamiento, duracion);
+            arbol.agregar_cancion(cancion); 
+        }
+
+        archivo.close();  
     }
-
-    archivo.close();  
-    }
-    else{
-        std::cerr << "Error al abrir el archivo" << std::endl; 
+    else {
+        std::cout << "Error al abrir el archivo" << std::endl; 
         // en caso de no poder abrir el archivo
         return 1; 
     }
-
 
     int option=0;
     int menu=1;
@@ -59,8 +59,7 @@ int main() {
         std::cout << "Menu: " << std::endl;
         std::cout << "1. Mostrar listado de canciones." << std::endl;
         std::cout << "2. Agregar cancion." << std::endl;
-        std::cout << "3. Ordenar inventario. " << std::endl;
-        std::cout << "4. Buscar cancion. " << std::endl;
+        std::cout << "3. Buscar cancion. " << std::endl;
         std::cout << "5. Salir del programa" << std:: endl;
         std::cout << "Elija una opcion : " ;
 
@@ -69,7 +68,7 @@ int main() {
         if (option == 1)
         {
             std::cout << "Listado de canciones de tu dispositivo: " << std::endl;
-            sorter.mostrar_lista();
+            std::cout<< arbol.inorder() << endl;
         }
 
         else if (option == 2)
@@ -94,99 +93,35 @@ int main() {
             std::cin >> vistas;
             std::cout << "Fecha de lanzamiento de la cancion: " << std::endl;
             std::cin >> lanzamiento;
-            sorter.agregar_cancion(Cancion(nombre,artista,genero,duracion,
+            arbol.agregar_cancion(Cancion(nombre,artista,genero,duracion,
             vistas,lanzamiento));
             std::cout << "Cancion agregada." << std::endl; 
             std::cout << "Deseas seguir agregando peliculas? (si=1 / no=0): ";
             std::cin >> fin;
             }
         }
-
-        else if (option == 3)
-        {
-            int opcion=1;
-            int option;
-            while (opcion == 1 )
-            {
-            std::cout << "Como quieres ordenar el inventario: " << std::endl;
-            std::cout << "1. Por orden alfabetico." << std::endl;
-            std::cout << "2. Por artista" << std::endl;
-            std::cout << "3. Por genero musical." << std::endl;
-            std::cout << "4. Por duracion de la cancion(min). " << std::endl;
-            std::cout << "5. Por popularidad " << std::endl;
-            std::cout << "6. Por fecha de lanzamiento" << std::endl;
-            std::cout << "Elija una opcion : " ;
-
-            std::cin >> option;
-
-            if(option==1){
-                sorter.ordena_nombre();
-                sorter.mostrar_lista();
-            }
-            else if(option==2){
-                sorter.ordena_artista();
-                sorter.mostrar_lista();
-            }
-            else if(option==3){
-                sorter.ordena_genero();
-                sorter.mostrar_lista();
-            }
-            else if(option==4){
-                sorter.ordena_duracion();
-                sorter.mostrar_lista();
-            }
-            else if(option==5){
-                sorter.ordena_vistas();
-                sorter.mostrar_lista();
-            }
-            else if(option==6){
-                sorter.ordena_lanzamiento();
-                sorter.mostrar_lista();
-            }
-            else{
-                std::cout << "Opcion invalida...." << std::endl;
-                std::cout << "Intenta nuevamente." << std::endl;
-            }
-
-            std::cout << "Deseas seguir ordenando? (si=1 / no=0): ";
-            std::cin >> opcion;
-            }
-        }
-        
-        else if (option == 4)
-        {
+        else if (option == 3) {
+            // Buscar una canción por nombre
             int opcion = 1;
 
-            while (opcion == 1)
-            {
+            while (opcion == 1) {
                 std::string nom_buscado;
 
                 std::cout << "BUSCAR CANCION" << std::endl;
                 std::cout << "Introduce el nombre de la cancion: ";
 
-                std::cin.ignore(std::numeric_limits
-                <std::streamsize>::max(), '\n');
-          
-                
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Ignorar entrada previa
                 std::getline(std::cin, nom_buscado); 
-                // Buscar usando búsqueda binaria
-                Cancion* encontrada = sorter.buscarPorNombre(nom_buscado);
+                
+                std::string resultado = arbol.encontrar_cancion(nom_buscado);
+                std::cout << resultado << std::endl;  // Se imprime el resultado de la búsqueda
 
-                // Comprobar si se encontró la película
-                if (encontrada != nullptr) 
-                {
-                    std::cout << "Cancion encontrada:" << std::endl;
-                    encontrada->print();
-                }
-                
-                else
-                {
-                    std::cout << "No se encontro la pelicula: " 
-                    << nom_buscado << std::endl;
-                }
-                
                 std::cout << "Deseas seguir buscando? (si=1 / no=0): ";
-                std::cin >> opcion;
+                while (!(std::cin >> opcion) || (opcion != 0 && opcion != 1)) {
+                    std::cin.clear();  // Limpiar el estado de error de cin
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Limpiar el buffer
+                    std::cout << "Opción no válida, intenta nuevamente (si=1 / no=0): ";
+                }
             }
         }
         else if (option == 5)
@@ -194,13 +129,6 @@ int main() {
             menu=0;
         }
     }
-
-
-
-
-
-    //sorter.ordena_nombre();
-    //sorter.mostrar_lista();
 
 
     return 0;
